1e2f09f446c5c49d22594bd588dfd20173b5ef93
deathbuycactus <131403532+deathbuycactus@users.noreply.github.com>
2026-04-20 16:23:40 +0000

    added reranking code

--- RAG_Pipeline.py
+++ RAG_Pipeline.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from openai import OpenAI
-
+from sentence_transformers import CrossEncoder
 # import your retrieval function from vectorDB.py
 from vectorDB import retrieve_context
 
+reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 
@@ -33,7 +34,19 @@ ANSWER:
 def rag_pipeline(query, k=4):
 
     # Top-k chunks
-    context, sources = retrieve_context(query, k=k)
+    results = st.session_state.collection.query(
+        query_texts=[query],
+        n_results=10
+    )
+
+    docs = results.get("documents", [[]])[0]
+    metas = results.get("metadatas", [[]])[0]
+
+    # Chunk rerank
+    docs, metas = rerank(query, docs, metas, top_n=4)
+
+    context = "\n\n---\n\n".join(docs)
+    sources = metas
 
     if not context:
         return "No relevant context found.", None
@@ -51,5 +64,24 @@ def rag_pipeline(query, k=4):
     answer = response.choices[0].message.content
 
     return answer, sources
+
+def rerank(query, docs, metadatas, top_n=4):
+
+    if not docs:
+        return [], []
+
+    pairs = [(query, doc) for doc in docs]
+
+    # Rerank scoring
+    scores = reranker.predict(pairs)
+    scored_items = list(zip(docs, metadatas, scores))
+    scored_items.sort(key=lambda x: x[2], reverse=True)
+
+    top_items = scored_items[:top_n]
+
+    reranked_docs = [item[0] for item in top_items]
+    reranked_meta = [item[1] for item in top_items]
+
+    return reranked_docs, reranked_meta
 
 
